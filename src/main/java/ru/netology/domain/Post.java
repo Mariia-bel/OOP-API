151f68b91cd3daf9133287690964fd996eb22835
Мария <airbelka@bk.ru>
2022-06-03 22:43:17 +0300

    classes describing questionnaire request objects to the server

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,13 @@
+package ru.netology.domain;
+
+public class Post {
+    public String name;
+    public String passport; // Используются спецсимволы
+    public String patronymic;
+    public String phone; // Используются спецсимволы
+    public String surname;
+    public boolean subscription; // true or false
+
+    public FormDate birthday;
+}
+
